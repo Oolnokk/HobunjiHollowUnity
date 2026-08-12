70795f28f7e36351de3e85c0c4d02c5a98a9f081
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2026-08-12 18:14:48 -0400

    Use Khymeryyan Roman font for local save flow

--- docs/js/local-save-folder.js
+++ docs/js/local-save-folder.js
@@ -1,4 +1,5 @@
 // Compatibility loader: persistence core + default save/load UX flow.
 // Kept at the historical path so existing pages do not need to change script order.
+document.write('<style>#localSaveStartupGate{font-family:"KhymeryyanRomanLetters+Numbers","DM Mono",ui-monospace,monospace!important}</style>');
 document.write('<script src="js/local-save-folder-core.js?v=20260812a"><\/script>');
 document.write('<script src="js/local-save-flow.js?v=20260812a"><\/script>');
