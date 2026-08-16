42d8ee4f929dd955faac4f7c33ae8b1c2281dee9
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2026-08-16 15:46:42 -0400

    Load Grehlr burrow combat module

--- docs/js/combat/combat-config-loader.js
+++ docs/js/combat/combat-config-loader.js
@@ -89,6 +89,7 @@
     ['js/drunk-prone-composition-bridge.js?v=20260812b', () => !!window.HobunjiDrunkProneCompositionBridge],
     ['js/prone-motion-exclusivity.js?v=20260812a', () => !!window.HobunjiProneMotionExclusivity],
     ['js/footing-damage-recovery-bridge.js?v=20260812a', () => !!window.HobunjiFootingDamageRecovery],
+    ['js/combat/combat-grehlr-burrow.js?v=20260816a', () => !!window.HobunjiGrehlrBurrow],
   ];
 
   function loadModule(src, alreadyLoaded) {
