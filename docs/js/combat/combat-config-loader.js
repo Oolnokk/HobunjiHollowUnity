5064797ebf4ec398f8335ab9f46e44e478b87f3b
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2026-09-03 00:03:53 -0400

    Load selective NPC old-age effects before gameplay init

--- docs/js/combat/combat-config-loader.js
+++ docs/js/combat/combat-config-loader.js
@@ -82,6 +82,7 @@
     ['js/fine-hood-trim-head-facing.js?v=20260824b', () => !!window.HobunjiFineHoodTrimHeadFacing],
     ['js/front-hat-head-facing.js?v=20260830flip2', () => !!window.HobunjiFrontHatHeadFacing],
     ['js/hat-xray-head-facing.js?v=20260824a', () => !!window.HobunjiHatXrayHeadFacing],
+    ['js/npc-old-age-effects.js?v=20260903a', () => !!window.HobunjiNpcOldAgeEffects],
     ['js/town-player-body-elevation-bridge.js?v=20260810a', () => !!window.HobunjiTownBodyElevationBridge],
     ['js/drunk-locomotion.js?v=20260812a', () => !!window.HobunjiDrunkWalk],
     ['js/crop-sprite-art.js?v=20260814a', () => !!window.HobunjiCropSpriteArt],
