e295f0c909a215a22474622b9c2173aa2e93a902
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2026-09-14 02:10:56 -0400

    Load Google Drive same-revision conflict guard

--- docs/js/local-save-folder.js
+++ docs/js/local-save-folder.js
@@ -6,5 +6,6 @@ document.write('<script src="js/save-snapshot-core.js?v=20260904a"><\/script>');
 document.write('<script src="js/local-save-folder-core.js?v=20260812a"><\/script>');
 document.write('<script src="js/google-drive-cloud-save-config.js?v=20260914a"><\/script>');
 document.write('<script src="js/google-drive-cloud-save.js?v=20260914a"><\/script>');
+document.write('<script src="js/google-drive-cloud-save-conflict-guard.js?v=20260914a"><\/script>');
 document.write('<script src="js/local-save-flow.js?v=20260812a"><\/script>');
 document.write('<script src="js/save-startup-gate.js?v=20260910review1"><\/script>');
