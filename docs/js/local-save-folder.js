ba1616563e4e56e305fa473e5a22a0eee82a5e36
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2026-09-14 10:03:32 -0400

    Accept empty folder as first-save destination

--- docs/js/local-save-folder.js
+++ docs/js/local-save-folder.js
@@ -6,6 +6,7 @@ document.write('<script src="js/session-persistence-startup-guard.js?v=20260913a
 document.write('<script src="js/save-snapshot-core.js?v=20260904a"><\/script>');
 document.write('<script src="js/local-save-folder-core.js?v=20260812a"><\/script>');
 document.write('<script src="js/folder-save-primary.js?v=20260914a"><\/script>');
+document.write('<script src="js/folder-save-empty-bootstrap.js?v=20260914a"><\/script>');
 document.write('<script src="js/netlify-cloud-save.js?v=20260904a"><\/script>');
 document.write('<script src="js/local-save-flow.js?v=20260812a"><\/script>');
 document.write('<script src="js/save-startup-gate.js?v=20260910review1"><\/script>');
