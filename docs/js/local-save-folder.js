3a86472ea645d04bc28c0121d1f726a7ac925f18
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2026-09-15 11:47:33 -0400

    Refresh conflict and identity probe script caches

--- docs/js/local-save-folder.js
+++ docs/js/local-save-folder.js
@@ -21,8 +21,8 @@ document.write('<script src="js/folder-save-runtime-flush.js?v=20260914a"><\/scr
 document.write('<script src="js/folder-save-quit-guard.js?v=20260915drivec"><\/script>');
 document.write('<script src="js/folder-save-debug-ui.js?v=20260915drivei"><\/script>');
 document.write('<script src="js/google-drive-save-ui.js?v=20260915a"><\/script>');
-document.write('<script src="js/google-drive-save-conflict-ui.js?v=20260915a"><\/script>');
-document.write('<script src="js/google-drive-save-identity-probe.js?v=20260915a"><\/script>');
+document.write('<script src="js/google-drive-save-conflict-ui.js?v=20260915b"><\/script>');
+document.write('<script src="js/google-drive-save-identity-probe.js?v=20260915b"><\/script>');
 document.write('<script src="js/google-drive-save-lifecycle.js?v=20260915a"><\/script>');
 document.write('<script src="js/google-drive-save-startup.js?v=20260915c"><\/script>');
 document.write('<script src="js/google-drive-save-legacy-cloud-bridge.js?v=20260915c"><\/script>');
