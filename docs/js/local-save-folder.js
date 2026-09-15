58ceb8961386d481ddf898152ab42412556fa414
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2026-09-15 10:55:41 -0400

    Wire Google Drive restore bridge into persistence bootstrap

--- docs/js/local-save-folder.js
+++ docs/js/local-save-folder.js
@@ -18,9 +18,10 @@ document.write('<script src="js/folder-save-empty-bootstrap.js?v=20260914a"><\/s
 document.write('<script src="js/folder-save-device-provenance.js?v=20260915drivec"><\/script>');
 document.write('<script src="js/folder-save-runtime-flush.js?v=20260914a"><\/script>');
 document.write('<script src="js/folder-save-quit-guard.js?v=20260915driveb"><\/script>');
-document.write('<script src="js/folder-save-debug-ui.js?v=20260915driveb"><\/script>');
+document.write('<script src="js/folder-save-debug-ui.js?v=20260915drivec"><\/script>');
 document.write('<script src="js/google-drive-save-ui.js?v=20260915a"><\/script>');
 document.write('<script src="js/google-drive-save-startup.js?v=20260915b"><\/script>');
+document.write('<script src="js/google-drive-save-legacy-cloud-bridge.js?v=20260915a"><\/script>');
 document.write('<script src="js/netlify-cloud-save.js?v=20260904a"><\/script>');
 document.write('<script src="js/local-save-flow.js?v=20260812a"><\/script>');
 document.write('<script src="js/save-startup-gate.js?v=20260910review1"><\/script>');
