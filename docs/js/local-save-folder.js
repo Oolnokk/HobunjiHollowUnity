a81bf0caa75972d219673449b47fd76117b90d57
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2026-09-15 22:02:01 -0400

    Refresh audited save diagnostics module

--- docs/js/local-save-folder.js
+++ docs/js/local-save-folder.js
@@ -11,7 +11,7 @@ document.write('<script src="js/folder-save-device-provenance.js?v=20260914a"><\
 document.write('<script src="js/folder-save-runtime-flush.js?v=20260914a"><\/script>');
 document.write('<script src="js/save-checkpoint-manager.js?v=20260915d"><\/script>');
 document.write('<script src="js/folder-save-quit-guard.js?v=20260914a"><\/script>');
-document.write('<script src="js/folder-save-debug-ui.js?v=20260914a"><\/script>');
+document.write('<script src="js/folder-save-debug-ui.js?v=20260915a"><\/script>');
 document.write('<script src="js/netlify-cloud-save.js?v=20260904a"><\/script>');
 document.write('<script src="js/local-save-flow.js?v=20260812a"><\/script>');
 document.write('<script src="js/save-startup-gate.js?v=20260910review1"><\/script>');
