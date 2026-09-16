4f25bd453a123e633979d4f89955f400724a9206
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2026-09-15 21:18:16 -0400

    Bust folder recovery script caches

--- docs/js/local-save-folder.js
+++ docs/js/local-save-folder.js
@@ -4,12 +4,12 @@ document.write('<style>#localSaveStartupGate{font-family:"KhymeryyanRomanLetters
 document.write('<link rel="stylesheet" href="folder-save-primary.css?v=20260914a">');
 document.write('<script src="js/session-persistence-startup-guard.js?v=20260913a"><\/script>');
 document.write('<script src="js/save-snapshot-core.js?v=20260904a"><\/script>');
-document.write('<script src="js/local-save-folder-core.js?v=20260812a"><\/script>');
+document.write('<script src="js/local-save-folder-core.js?v=20260915b"><\/script>');
 document.write('<script src="js/folder-save-primary.js?v=20260914a"><\/script>');
 document.write('<script src="js/folder-save-empty-bootstrap.js?v=20260914a"><\/script>');
 document.write('<script src="js/folder-save-device-provenance.js?v=20260914a"><\/script>');
 document.write('<script src="js/folder-save-runtime-flush.js?v=20260914a"><\/script>');
-document.write('<script src="js/save-checkpoint-manager.js?v=20260915a"><\/script>');
+document.write('<script src="js/save-checkpoint-manager.js?v=20260915b"><\/script>');
 document.write('<script src="js/folder-save-quit-guard.js?v=20260914a"><\/script>');
 document.write('<script src="js/folder-save-debug-ui.js?v=20260914a"><\/script>');
 document.write('<script src="js/netlify-cloud-save.js?v=20260904a"><\/script>');
