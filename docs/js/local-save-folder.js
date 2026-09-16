d50cac28a0c3c3e89ce162fcf08304b93c3b7812
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2026-09-16 12:53:07 -0400

    Bump guarded menu control loader

--- docs/js/local-save-folder.js
+++ docs/js/local-save-folder.js
@@ -10,7 +10,7 @@ document.write('<script src="js/folder-save-empty-bootstrap.js?v=20260914a"><\/s
 document.write('<script src="js/folder-save-device-provenance.js?v=20260914a"><\/script>');
 document.write('<script src="js/folder-save-runtime-flush.js?v=20260914a"><\/script>');
 document.write('<script src="js/save-checkpoint-manager.js?v=20260915d"><\/script>');
-document.write('<script src="js/folder-save-quit-guard.js?v=20260914a"><\/script>');
+document.write('<script src="js/folder-save-quit-guard.js?v=20260916menu1"><\/script>');
 document.write('<script src="js/folder-save-debug-ui.js?v=20260915a"><\/script>');
 document.write('<script src="js/netlify-cloud-save.js?v=20260904a"><\/script>');
 document.write('<script src="js/local-save-flow.js?v=20260812a"><\/script>');
