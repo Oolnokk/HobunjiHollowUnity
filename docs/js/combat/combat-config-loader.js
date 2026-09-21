89a2f98a5b81684e46feba1d74cfdf22315db578
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2026-09-20 21:20:35 -0400

    Bump spear throw archetype contract

--- docs/js/combat/combat-config-loader.js
+++ docs/js/combat/combat-config-loader.js
@@ -123,7 +123,7 @@
     ['js/combat/enemy-target-facing.js?v=20260906a', () => Number(window.EnemyTargetFacing?.version) >= 1],
     ['js/combat/enemy-weapon-stances.js?v=20260903a', () => !!window.EnemyWeaponStances],
     ['js/npc-held-equipment-v4.js?v=20260920orbit1', () => Number(window.NpcHeldEquipment?.version) >= 4],
-    ['js/combat/ranged-weapon-archetypes.js?v=20260920projectileport1-arcembed1-spearspin1', () => Number(window.HobunjiRangedWeaponArchetypes?.version) >= 12],
+    ['js/combat/ranged-weapon-archetypes.js?v=20260920projectileport1-arcembed1-spearspin2', () => Number(window.HobunjiRangedWeaponArchetypes?.version) >= 13],
     ['js/combat/ranged-camera-ray-authority.js?v=20260909perspectivepoint1', () => Number(window.HobunjiRangedCameraRayAuthority?.version) >= 2],
     ['js/combat/ranged-camera-focus.js?v=20260920rangecameraorbit2', () => Number(window.HobunjiRangedCameraFocus?.version) >= 10],
     ['js/combat/combat-camera-alignment-bridge.js?v=20260917charge3', () => Number(window.HobunjiCombatCameraAlignment?.version) >= 4],
