dc9ab534ff02d60c1d201cbd874f7b87999b04c7
Oolnokk <155416726+Oolnokk@users.noreply.github.com>
2026-09-21 11:25:11 -0400

    Bust inventory UI cache for woven icon refresh

--- docs/js/equipment-panel.js
+++ docs/js/equipment-panel.js
@@ -76,7 +76,7 @@
     inventoryUiLoadStarted = true;
     // Versioned source is loaded here so Pack presentation can be decoupled without editing game.js or index.html.
     const script = document.createElement('script');
-    script.src = 'js/inventory-ui.js?v=20260812c';
+    script.src = 'js/inventory-ui.js?v=20260921woven-icons1';
     script.async = false;
     script.onload = () => window.InventoryUI?.init?.(inventoryUiDeps());
     script.onerror = () => {
